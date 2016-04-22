eeac1aff82aedb4fde24a8ab328c4bc45a63b4b0
Eleni Chourouzidou <eleni@webspirit.gr>
2016-04-23 01:03:49 +0300

    Fixed timezone permitted param, links in header changed, styled the profile page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'pages#home'
 
   get 'pages/home'
-  devise_for :users, controller: { registrations: 'registrations' }
+  devise_for :users, controllers: { registrations: 'registrations' }
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
